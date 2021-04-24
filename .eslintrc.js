7e85353baefd6a3225d7a89e22b04327e71512bb
George Bockari <bitwhys@Georges-Mac-mini.local>
2021-04-23 19:15:40 -0700

    chore: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'airbnb',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-filename-extension': 0,
+  },
+}
---
+++ pages/index.js
@@ -0,0 +1,6 @@
+import Head from 'next/head'
+import Dashboard from '../components/Dashboard'
+
+export default function Home() {
+  return <Dashboard />
+}
